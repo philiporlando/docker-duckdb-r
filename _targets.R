edc4c1196f2b8163970769a06f6ffbda7cd2e78d
philiporlando <phlp.orlando@gmail.com>
2024-08-03 21:53:49 -0700

    add targets to download and ingest medi-cal provider data

--- _targets.R
+++ _targets.R
@@ -1,32 +1,38 @@
-# Created by use_targets().
-# Follow the comments below to fill in this target script.
-# Then follow the manual to check and run the pipeline:
-#   https://books.ropensci.org/targets/walkthrough.html#inspect-the-pipeline
-
-# Load packages required to define the pipeline:
 library(targets)
 library(qs)
-# library(tarchetypes) # Load other packages as needed.
 
-# Set target options:
+
 targets::tar_option_set(
   packages = c("tibble"),
   format = "qs"
   # controller = crew::crew_controller_local(workers = 2, seconds_idle = 60)
 )
 
-# Run the R scripts in the R/ folder with your custom functions:
-# tar_source()
-# tar_source("other_functions.R") # Source other scripts as needed.
+targets::tar_source()
 
-# Replace the target list below with your own:
 list(
   tar_target(
-    name = data,
-    command = tibble(x = rnorm(100), y = rnorm(100)) 
+    name = url,
+    command = get_medi_cal_managed_care_providers_url()
   ),
   tar_target(
-    name = model,
-    command = coefficients(lm(y ~ x, data = data))
+    name = url_modified_time,
+    command = get_url_modified_time(url)
+  ),
+  tar_target(
+    name = medi_cal_managed_care_providers_file,
+    command = download_medi_cal_managed_care_providers(url, url_modified_time),
+    format = "file",
+  ),
+  tar_target(
+    name = duckdb_file,
+    command = setup_duckdb()
+  ),
+  tar_target(
+    name = ingested_medi_cal_managed_care_providers,
+    command = ingest_medi_cal_managed_care_providers(
+      medi_cal_managed_care_providers_file,
+      duckdb_file
+    )
   )
 )
